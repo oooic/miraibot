0e89ef9fe8882dedd2170597fb0137b984b25f2f
Koki Muraoka <muraok_k@chemsys.t.u-tokyo.ac.jp>
2022-07-17 07:55:23 +0900

    :zap:

--- bot.py
+++ bot.py
@@ -1,24 +1,24 @@
-import subprocess
-import pandas as pd
-from io import StringIO
-import paramiko
-import socket
-import requests
 import json
 import os
+import socket
+from io import StringIO
+
+import pandas as pd
+import paramiko
+import requests
 from paramiko_expect import SSHClientInteraction
 
 PROMPT = "(\([\-0-9A-z_]+\)\s)?~\s>\s"  # noqa: W605
 
 
-user = os.environ['SSH_USER']
+user = os.environ["SSH_USER"]
 
-host = os.environ['SSH_GATEWAY_HOST']
-machine = os.environ['SSH_MACHINE']
+host = os.environ["SSH_GATEWAY_HOST"]
+machine = os.environ["SSH_MACHINE"]
 
 
-def post_slack(text):
-    WEB_HOOK_URL = os.environ['WEB_HOOK_URL']
+def post_slack(text: str) -> None:
+    WEB_HOOK_URL = os.environ["WEB_HOOK_URL"]
     requests.post(
         WEB_HOOK_URL,
         data=json.dumps(
@@ -31,37 +31,18 @@ def post_slack(text):
     )
 
 
-def get_output(cmd, shell=False):
-    p = subprocess.Popen(
-        cmd,
-        stdout=subprocess.PIPE,
-        stdin=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        shell=shell
-    )
-
-    out,  err = p.communicate()
-    out = out.decode("utf-8")
-    err = err.decode("utf-8")
-
-    return out
-
-
-def get_output(command):
+def get_output(command: str) -> None:
     proxy = paramiko.ProxyCommand(f"ssh {user}@{host} -p 22 nc {machine} 22")
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
-    client.connect(
-        machine,
-        port=22,
-        username=user,
-        sock=proxy
-    )
+    client.connect(machine, port=22, username=user, sock=proxy)
 
     def output(x):
         return None
 
-    with SSHClientInteraction(client, timeout=10, display=True, output_callback=output, tty_width=250) as interact:
+    with SSHClientInteraction(
+        client, timeout=10, display=True, output_callback=output, tty_width=250
+    ) as interact:
         interact.send("")
         interact.expect(PROMPT)
         txt = ""
@@ -74,8 +55,7 @@ def get_output(command):
 
 
 def my_update():
-    cmd = ["/usr/sge/bin/linux-x64/qstat",
-           "-u", user, "|", "grep", "-v", "LowPri"]
+    cmd = ["/usr/sge/bin/linux-x64/qstat", "-u", user, "|", "grep", "-v", "LowPri"]
     cmd = " ".join(cmd)
 
     my_mirai = get_output(cmd)
@@ -94,17 +74,28 @@ def my_update():
         post_slack(mirai)
 
 
-def
-
-
 def memory_usage():
     qhost = get_output("/usr/sge/bin/linux-x64/qhost")
-    df = pd.read_csv(StringIO(qhost), skiprows=3, sep="\s+", names=[
-                     'node', 'os', 'cores', 'load', 'max_mem', 'used_mem', 'max_swap', 'used_swap'])
+    df = pd.read_csv(
+        StringIO(qhost),
+        skiprows=3,
+        sep="\s+",
+        names=[
+            "node",
+            "os",
+            "cores",
+            "load",
+            "max_mem",
+            "used_mem",
+            "max_swap",
+            "used_swap",
+        ],
+    )
 
-    for mem in 'max_mem', 'used_mem', 'max_swap', 'used_swap':
-        df.loc[:, mem] = df[mem].str.replace(
-            "M", "e3").str.replace("G", "e6").astype(float)
+    for mem in "max_mem", "used_mem", "max_swap", "used_swap":
+        df.loc[:, mem] = (
+            df[mem].str.replace("M", "e3").str.replace("G", "e6").astype(float)
+        )
 
     df.used_mem / df.max_mem > 0.9
 
@@ -112,19 +103,29 @@ def memory_usage():
 
     qstat = get_output("/usr/sge/bin/linux-x64/qstat | tail -n +3")
 
-    df_qstat = pd.read_csv(StringIO(qstat), sep="\s+", names=['jobID',  'prior', 'name', 'user',  'state', 'date', 'time', 'queue', 'slots'])
+    df_qstat = pd.read_csv(
+        StringIO(qstat),
+        sep="\s+",
+        names=[
+            "jobID",
+            "prior",
+            "name",
+            "user",
+            "state",
+            "date",
+            "time",
+            "queue",
+            "slots",
+        ],
+    )
 
-    df_qstat['node'] = df_qstat.queue.str.split("@").str[1]
+    df_qstat["node"] = df_qstat.queue.str.split("@").str[1]
 
-    merged_df = high_memory.merge(df_qstat, how='inner')
-
+    merged_df = high_memory.merge(df_qstat, how="inner")
 
     msg = ""
     for _, row in merged_df.iterrows():
         f"@{row.name}\nJOB ID{row.jobID}"
-
-
-
 
 
 def main():
