b35311665a97cdc790ca305419fa08b08b8498f8
Koki Muraoka <muraok_k@chemsys.t.u-tokyo.ac.jp>
2022-07-17 08:20:03 +0900

    :zap:

--- bot.py
+++ bot.py
@@ -128,7 +128,7 @@ def memory_usage():
 
     msg = ""
     for _, row in merged_df.iterrows():
-        msg += f"@{row.name}\n{row.queue}のジョブ#{row.jobID}が"
+        msg += f"@{row.user}\n{row.queue}のジョブ#{row.jobID}が"
         msg += f"{row.MEMUSE:.3g}%ものメモリを消費してしまっています。"
         msg += "低速化やクラッシュの恐れがあります。\n"
 
