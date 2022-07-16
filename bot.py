625c10ba858e35bb4cb701beb9416790279dd3e3
Koki Muraoka <muraok_k@chemsys.t.u-tokyo.ac.jp>
2022-07-17 07:44:55 +0900

    :zap:

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 import subprocess
+import pandas as pd
+from io import StringIO
 import paramiko
 import socket
 import requests
@@ -45,8 +47,7 @@ def get_output(cmd, shell=False):
     return out
 
 
-def check_loop():
-
+def get_output(command):
     proxy = paramiko.ProxyCommand(f"ssh {user}@{host} -p 22 nc {machine} 22")
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
@@ -57,32 +58,78 @@ def check_loop():
         sock=proxy
     )
 
+    def output(x):
+        return None
 
-    with SSHClientInteraction(client, timeout=10, display=True, tty_width=250) as interact:
+    with SSHClientInteraction(client, timeout=10, display=True, output_callback=output, tty_width=250) as interact:
         interact.send("")
         interact.expect(PROMPT)
         txt = ""
 
-        cmd = ["/usr/sge/bin/linux-x64/qstat",  "-u", user, "|", "grep", "-v", "LowPri"]
-        cmd = " ".join(cmd)
-
-        interact.send(cmd)
+        interact.send(command)
         interact.expect(PROMPT)
 
         mirai = interact.current_output
+        return mirai
 
-        mirai = "`mirai updates:`\n```\n" + mirai + "```\n"
 
-        mirai_last = ""
-        if os.path.exists("mirai.txt"):
-            with open("mirai.txt") as f:
-                mirai_last = f.read()
+def my_update():
+    cmd = ["/usr/sge/bin/linux-x64/qstat",
+           "-u", user, "|", "grep", "-v", "LowPri"]
+    cmd = " ".join(cmd)
 
-        with open("mirai.txt", "w") as f:
-            f.write(mirai)
+    my_mirai = get_output(cmd)
 
-        if mirai != mirai_last:
-            post_slack(mirai)
+    my_mirai = "`mirai updates:`\n```\n" + my_mirai + "```\n"
+
+    mirai_last = ""
+    if os.path.exists("mirai.txt"):
+        with open("mirai.txt") as f:
+            mirai_last = f.read()
+
+    with open("mirai.txt", "w") as f:
+        f.write(mirai)
+
+    if mirai != mirai_last:
+        post_slack(mirai)
+
+
+def
+
+
+def memory_usage():
+    qhost = get_output("/usr/sge/bin/linux-x64/qhost")
+    df = pd.read_csv(StringIO(qhost), skiprows=3, sep="\s+", names=[
+                     'node', 'os', 'cores', 'load', 'max_mem', 'used_mem', 'max_swap', 'used_swap'])
+
+    for mem in 'max_mem', 'used_mem', 'max_swap', 'used_swap':
+        df.loc[:, mem] = df[mem].str.replace(
+            "M", "e3").str.replace("G", "e6").astype(float)
+
+    df.used_mem / df.max_mem > 0.9
+
+    high_memory = df[df.used_mem / df.max_mem > 0.7]
+
+    qstat = get_output("/usr/sge/bin/linux-x64/qstat | tail -n +3")
+
+    df_qstat = pd.read_csv(StringIO(qstat), sep="\s+", names=['jobID',  'prior', 'name', 'user',  'state', 'date', 'time', 'queue', 'slots'])
+
+    df_qstat['node'] = df_qstat.queue.str.split("@").str[1]
+
+    merged_df = high_memory.merge(df_qstat, how='inner')
+
+
+    msg = ""
+    for _, row in merged_df.iterrows():
+        f"@{row.name}\nJOB ID{row.jobID}"
+
+
+
+
+
+def main():
+    my_update()
+
 
 if __name__ == "__main__":
-    check_loop()
+    main()
